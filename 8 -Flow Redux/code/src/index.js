87e86bdbbdfc9e9bfe62efa43e66e0b8c4341481
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-10-14 04:55:01 +0700

    setup redux Flow Redux

--- 8 -Flow Redux/code/src/index.js
+++ 8 -Flow Redux/code/src/index.js
@@ -7,11 +7,13 @@ import "./index.css";
 import configureStore from "./redux/configureStore";
 import { Provider as ReduxProvider } from "react-redux";
 
+const store = configureStore();
+
 render(
-    <ReduxProvider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </ReduxProvider>,
-    document.getElementById("app")
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
+  document.getElementById("app")
 );
--- Flow Redux/code/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducer } from "redux";
-import courseReducer from "./courseReducer";
-
-const rootReducer = combineReducer({
-    course
-});
-
-export default rootReducer;
